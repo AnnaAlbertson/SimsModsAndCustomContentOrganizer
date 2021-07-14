03a7a036ea0af6f139a227f1e1cb1f7dd3059f54
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-07-14 09:11:39 -0400

    Custom Content entity

--- SMACCO.Data/Entities/CustomContent.cs
+++ SMACCO.Data/Entities/CustomContent.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SMACCO.Data.Entities
 {
-    class CustomContent
+    public class CustomContent
     {
+        [Key]
+        public int CustomContentID { get; set; }
+
+        [Required]
+        [Display(Name = "Is this a Buy Mode item?")]
+        public bool IsBuyMode { get; set; }
+
+        [Required]
+        [Display(Name = "Is this a Create-A-Sim item?")]
+        public bool IsCreateASim { get; set; }
+
+        public string Category { get; set; }
+        public string Subcategory { get; set; }
+
+        [ForeignKey(nameof(Downloads))]
+        public int DownloadID { get; set; }
+        public virtual Download Downloads { get; set; }
+
+        // Picture of Content
     }
 }
