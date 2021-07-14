4d76f7c3493d69507cc9bc5082f5ff83b818f319
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-07-14 07:12:31 -0400

    Identity model methods built

--- SMACCO.Data/IdentityModels.cs
+++ SMACCO.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -38,5 +40,32 @@ namespace SMACCO.Data
         // public DbSet<Download> Downloads { get; set; }
         // public DbSet<Mod> Mods { get; set; }
         // public DbSet<CustomContent> CustomContents { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
+        }
     }
 }
