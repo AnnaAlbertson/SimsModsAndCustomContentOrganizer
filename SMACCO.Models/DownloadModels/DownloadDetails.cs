63d0d999b3ad11d8f56c6ff8e14b9d2a49f56ba1
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-07-14 19:37:42 -0400

    DownloadDetails model

--- SMACCO.Models/DownloadModels/DownloadDetails.cs
+++ SMACCO.Models/DownloadModels/DownloadDetails.cs
@@ -8,5 +8,17 @@ namespace SMACCO.Models.DownloadModels
 {
     public class DownloadDetails
     {
+        public int DownloadID { get; set; }
+        public string DownloadName { get; set; }
+        public string CreatorName { get; set; }
+        public string DownloadURL { get; set; }
+        public DateTime LastDownloaded { get; set; }
+        public bool IsMod { get; set; }
+        public bool IsCustomContent { get; set; }
+        public string GameName { get; set; }
+
+        //public List<PackModels.PackListItem> ListOfPacks { get; set; }
+        //public List<ModModels.ModListItem> ListOfMods { get; set; }
+        //public List<CustomContentModels.CustomContentListItem> ListOfCustomContent { get; set; }
     }
 }
