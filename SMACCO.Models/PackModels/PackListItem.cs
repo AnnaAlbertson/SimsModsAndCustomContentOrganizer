eed16eb759d135d657a08d2d7f2fce2c2d32a0d0
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-07-14 19:19:01 -0400

    PackListItem model

--- SMACCO.Models/PackModels/PackListItem.cs
+++ SMACCO.Models/PackModels/PackListItem.cs
@@ -8,5 +8,9 @@ namespace SMACCO.Models.PackModels
 {
     public class PackListItem
     {
+        public string PackName { get; set; }
+        public string Description { get; set; }
+        public bool IsOwned { get; set; }
+        public string GameName { get; set; }
     }
 }
