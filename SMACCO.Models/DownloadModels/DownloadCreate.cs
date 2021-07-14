fca973f88f301e9f656569eb4a880b9bfd36fb20
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-07-14 19:24:20 -0400

    DownloadCreate model

--- SMACCO.Models/DownloadModels/DownloadCreate.cs
+++ SMACCO.Models/DownloadModels/DownloadCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace SMACCO.Models.DownloadModels
 {
     public class DownloadCreate
     {
+        public string DownloadName { get; set; }
+        public string CreatorName { get; set; }
+        public string DownloadURL { get; set; }
+        public DateTime LastDownloaded { get; set; }
+        public bool IsMod { get; set; }
+        public bool IsCustomContent { get; set; }
+        public int GameID { get; set; }
     }
 }
