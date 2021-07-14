16aeea93e4a0c9ef4ab1771d8524658c3a1eaaa6
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-07-14 19:16:32 -0400

    PackEdit model

--- SMACCO.Models/PackModels/PackEdit.cs
+++ SMACCO.Models/PackModels/PackEdit.cs
@@ -8,5 +8,9 @@ namespace SMACCO.Models.PackModels
 {
     public class PackEdit
     {
+        public string PackName { get; set; }
+        public string Description { get; set; }
+        public bool IsOwned { get; set; }
+        public int GameID { get; set; }
     }
 }
