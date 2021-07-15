3317851cfeb5851aad888dece0afd1195977183e
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-07-15 19:36:55 -0400

    Started CreateGame method

--- SMACCO.Services/Services/GameService.cs
+++ SMACCO.Services/Services/GameService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SMACCO.Data;
+using SMACCO.Data.Entities;
+using SMACCO.Models.GameModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,19 @@ namespace SMACCO.Services.Services
 {
     public class GameService
     {
+        public bool CreateGame(GameCreate model)
+        {
+            var entity =
+                new Game()
+                {
+                    GameName = model.GameName,
+                    IsOwned = model.IsOwned,
+                    LastPatchUpdate = model.LastPatchUpdate
+                };
+            using (var sdx = new ApplicationDbContext())
+            {
+                
+            }
+        }
     }
 }
