6979e00dcd994413d1e392a398b68f7c4559bc82
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-07-15 16:49:33 -0400

    CustomContentCreate model

--- SMACCO.Models/CustomContent Models/CustomContentCreate.cs
+++ SMACCO.Models/CustomContent Models/CustomContentCreate.cs
@@ -8,5 +8,10 @@ namespace SMACCO.Models.CustomContent_Models
 {
     public class CustomContentCreate
     {
+        public int CustomContentID { get; set; }
+        public bool IsBuyMode { get; set; }
+        public bool IsCreateASim { get; set; }
+        public string Category { get; set; }
+        public string Subcategory { get; set; }
     }
 }
