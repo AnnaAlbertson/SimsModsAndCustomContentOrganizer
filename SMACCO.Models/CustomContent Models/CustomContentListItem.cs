824bfdd4eb3e674349ad592abb2aa42be05f9d79
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-07-15 16:52:12 -0400

    CustomContentListItem model

--- SMACCO.Models/CustomContent Models/CustomContentListItem.cs
+++ SMACCO.Models/CustomContent Models/CustomContentListItem.cs
@@ -8,5 +8,11 @@ namespace SMACCO.Models.CustomContent_Models
 {
     public class CustomContentListItem
     {
+        public int CustomContentID { get; set; }
+        public string DownloadName { get; set; }
+        public bool IsBuyMode { get; set; }
+        public bool IsCreateASim { get; set; }
+        public string Category { get; set; }
+        public string Subcategory { get; set; }
     }
 }
