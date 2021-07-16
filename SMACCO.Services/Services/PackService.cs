5b9a0b37c68bca81a88cb0beab480d9c420dc788
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-07-16 18:02:13 -0400

    PackCreate method in PackService

--- SMACCO.Services/Services/PackService.cs
+++ SMACCO.Services/Services/PackService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SMACCO.Data;
+using SMACCO.Data.Entities;
+using SMACCO.Models.PackModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,101 @@ namespace SMACCO.Services.Services
 {
     public class PackService
     {
+        // private readonly Guid _userID;
+
+        //public NoteService(Guid userID)
+        //{
+        //  _userID = userID
+        //}
+
+        public bool CreatePack(PackCreate model)
+        {
+            var entity =
+                new Pack()
+                {
+                    //OwnerID or AdminID
+                    PackName = model.PackName,
+                    Description = model.PackName,
+                    GameID = model.GameID
+                };
+            using (var sdx = new ApplicationDbContext())
+            {
+                sdx.Packs.Add(entity);
+                return sdx.SaveChanges() == 1;
+            }
+        }
+
+        //public IEnumerable<GameListItem> GetGames()
+        //{
+        //    using (var sdx = new ApplicationDbContext())
+        //    {
+        //        var query =
+        //            sdx
+        //            .Games
+        //            .Select(
+        //                e =>
+        //                new GameListItem
+        //                {
+        //                    GameID = e.GameID,
+        //                    GameName = e.GameName,
+        //                    IsOwned = e.IsOwned,
+        //                    LastPatchUpdate = e.LastPatchUpdate
+        //                    //NumberOfDownloads = 
+        //                }
+        //                );
+        //        return query.ToArray();
+        //    }
+        //}
+
+        //public GameDetails GetGameByID(int id)
+        //{
+        //    using (var sdx = new ApplicationDbContext())
+        //    {
+        //        var entity =
+        //            sdx
+        //            .Games
+        //            .Single(e => e.GameID == id);  //&& OwnerID == _userID)
+        //        return
+        //            new GameDetails
+        //            {
+        //                GameID = entity.GameID,
+        //                GameName = entity.GameName,
+        //                IsOwned = entity.IsOwned,
+        //                LastPatchUpdate = entity.LastPatchUpdate
+        //            };
+        //    }
+        //}
+
+        //public bool UpdateGame(GameEdit model)
+        //{
+        //    using (var sdx = new ApplicationDbContext())
+        //    {
+        //        var entity =
+        //            sdx
+        //            .Games
+        //            .Single(e => e.GameID == model.GameID); //&&OwnerID == _userID
+
+        //        entity.GameName = model.GameName;
+        //        entity.IsOwned = model.IsOwned;
+        //        entity.LastPatchUpdate = model.LastPatchUpdate;
+
+        //        return sdx.SaveChanges() == 1;
+        //    }
+        //}
+
+        //public bool DeleteGame(int gameID)
+        //{
+        //    using (var sdx = new ApplicationDbContext())
+        //    {
+        //        var entity =
+        //            sdx
+        //            .Games
+        //            .Single(e => e.GameID == gameID); //OwnerID == _userID
+
+        //        sdx.Games.Remove(entity);
+
+        //        return sdx.SaveChanges() == 1;
+        //    }
+        //}
     }
 }
