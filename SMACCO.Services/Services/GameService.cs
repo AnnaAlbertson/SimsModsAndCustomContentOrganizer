f7fd4f1b20c9421d6ea6ddcf4fb05ce77e8a779a
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-07-16 13:27:01 -0400

    Coded GameCreate and GetGames methods in service layer

---
+++ SMACCO.Services/Services/GameService.cs
@@ -0,0 +1,75 @@
+﻿using SMACCO.Data;
+using SMACCO.Data.Entities;
+using SMACCO.Models.GameModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMACCO.Services.Services
+{
+    public class GameService
+    {
+        // private readonly Guid _userID;
+
+        //public NoteService(Guid userID)
+        //{
+        //  _userID = userID
+        //}
+
+        public bool CreateGame(GameCreate model)
+        {
+            var entity =
+                new Game()
+                {
+                    //OwnerID or AdminID
+                    GameName = model.GameName,
+                    IsOwned = model.IsOwned,
+                    LastPatchUpdate = model.LastPatchUpdate
+                };
+            using (var sdx = new ApplicationDbContext())
+            {
+                sdx.Games.Add(entity);
+                return sdx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<GameListItem> GetGames()
+        {
+            using (var sdx = new ApplicationDbContext())
+            {
+                var query =
+                    sdx
+                    .Games
+                    .Select(
+                        e =>
+                        new GameListItem
+                        {
+                            GameID = e.GameID,
+                            GameName = e.GameName,
+                            IsOwned = e.IsOwned,
+                            LastPatchUpdate = e.LastPatchUpdate
+                            //NumberOfDownloads = 
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+
+        public GameDetails GetGameByID(int id)
+        {
+
+        }
+
+        public bool UpdateGame(GameEdit model)
+        {
+
+        }
+
+        public bool DeleteGame(int gameID)
+        {
+
+        }
+    }
+}
