31bfd7ba35a5761e39d301dddf10add987b03151
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-08-10 13:15:25 -0400

    DownloadController Index method

---
+++ SMACCO/Controllers/CustomDataControllers/DownloadController.cs
@@ -0,0 +1,19 @@
+﻿using SMACCO.Models.DownloadModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMACCO.Controllers.CustomDataControllers
+{
+    public class DownloadController : Controller
+    {
+        // GET: Download
+        public ActionResult Index()
+        {
+            var model = new DownloadListItem[0];
+            return View(model);
+        }
+    }
+}
