abf5b05a481edf18eb861424f2b0d2ebd7a97371
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-08-10 13:42:34 -0400

    Create method in download controller

--- SMACCO/Controllers/CustomDataControllers/DownloadController.cs
+++ SMACCO/Controllers/CustomDataControllers/DownloadController.cs
@@ -7,12 +7,31 @@ using System.Web.Mvc;
 
 namespace SMACCO.Controllers.CustomDataControllers
 {
+    [Authorize]
     public class DownloadController : Controller
     {
-        // GET: Download
+        // GET: Download/Index
         public ActionResult Index()
         {
             var model = new DownloadListItem[0];
+            return View(model);
+        }
+
+        // GET: Download/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // HttpPost validation
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DownloadCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
             return View(model);
         }
     }
